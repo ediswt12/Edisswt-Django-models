38a61c290f32f8502d3d38eab530edb0f52840cc
Edile Memmedli <edisim7276@gmail.com>
2025-10-25 15:39:55 +0400

    Django models

---
+++ Django my project/djangotutorial/mysite/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/polls/')),  # root URL yönləndirmə
+]
